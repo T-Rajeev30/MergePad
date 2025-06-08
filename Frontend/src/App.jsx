73b60490c1ae5b03111d09d50978e09f28eee9ef
T-Rajeev30 <rajeevtiwariunder100@gmail.com>
2025-06-08 18:17:55 +0530

    Setup of Daisy ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <div className="bg-red-800 h-screen w-full">testing </div>
+      <div className="bg-red-800 h-screen w-full">
+        <button className="btn">Button</button>
+        <button className="btn btn-neutral">Neutral</button>
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-secondary">Secondary</button>
+        <button className="btn btn-accent">Accent</button>
+        <button className="btn btn-ghost">Ghost</button>
+        <button className="btn btn-link">Link</button>
+      </div>
     </>
   );
 }
